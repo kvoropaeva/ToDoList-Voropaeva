9b22280b69a2256be6bba12ba3299ed5fc7b64a3
Ksenia Voropaeva <k.m.voropaeva@gmail.com>
2017-12-24 22:46:20 +0300

    ToDoList (4)

--- ToDoList/Contracts/TaskModel.cs
+++ ToDoList/Contracts/TaskModel.cs
@@ -40,7 +40,10 @@ namespace Contracts
             private int _number;
             public int Number
             {
-                get { return _number; }
+                get
+            {
+                return _number;
+            }
                 set
                 {
                     _number = value;
